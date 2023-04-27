78d4a45e235f938d7203e0a3b0fd7f5f272d3cd5
alminasilnovic <116261786+alminasilnovic@users.noreply.github.com>
2023-04-27 14:17:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿var numbers = new int[1000000];
+var newnumbers = new int[999999];
+Random rand = new Random(4711);
+
+for (int i = 0; i < 1000000; i++)
+{
+    numbers[i] = rand.Next(1, 1000000001);
+}
+
+Array.Sort(numbers);
+
+for (int i = 1; i < numbers.Length; i++)
+{
+    newnumbers[i - 1] = numbers[i] - numbers[i - 1];
+}
+
+double average = newnumbers.Average();
+Console.WriteLine($"average: {average}");
